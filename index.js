9b3ec1bcb091a333a98aad749065a05b9ef4b2e1
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-10-17 10:02:00 -0400

    feat: initial commitlinter config

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+	parserPreset: 'conventional-changelog-conventionalcommits',
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat",
+				"fix",
+				"cont",
+				"chore"
+			]
+		  ]
+	}
+};
